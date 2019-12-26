b3d99e1f3db5318ec10a277e24f89df6a6ca2af6
Louis Navarro <azuiops.tv@gmail.com>
2019-12-26 22:44:35 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,147 @@
+import random
+import pygame as pg
+
+pg.init()
+
+###########
+# CLASSES #
+###########
+
+
+class Bird:
+    def __init__(self):
+        self.x = 150
+        self.y = 300
+
+        self.vel = 5
+
+        self.jumps = 0
+
+        self.pressed_space = False
+
+    def update(self):
+        if self.jumps:
+            if self.jumps < 6:
+                self.jumps += 1
+
+            elif self.jumps <= 10:
+                self.jumps += 1
+                self.vel += 1
+
+            elif self.jumps < 15:
+                self.jumps += 1
+
+            elif self.jumps == 15:
+                self.jumps = 0
+
+        elif self.vel < 7:
+            self.vel += 1
+
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_SPACE] and not (self.jumps or self.pressed_space):
+            self.pressed_space = True
+            self.vel = -6
+            self.jumps += 1
+
+        elif not keys[pg.K_SPACE]:
+            self.pressed_space = False
+
+        self.y += self.vel
+
+    def draw(self, win):
+        pg.draw.circle(win, (255, 0, 0), (self.x, self.y), 10)
+
+
+class Pipe:
+    def __init__(self, x=400):
+        self.len = random.randint(50, 350)
+
+        self.x = x
+
+    def update(self):
+        self.x -= 2
+
+    def draw(self, win):
+        pg.draw.rect(win, (0, 255, 0), (self.x, 0, 20, self.len))
+
+
+class PipePair(Pipe):
+    def __init__(self, pair, x=400):
+        super().__init__(x)
+        self.top = pair.len + 150
+
+    def draw(self, win):
+        pg.draw.rect(win, (0, 255, 0), (self.x, self.top, 20, 600 - self.top))
+
+
+#############
+# FUNCTIONS #
+#############
+
+
+def create_pipes():
+    pipe = Pipe()
+    pair = PipePair(pipe)
+
+    return pipe, pair
+
+
+def update():
+    bird.update()
+
+    if pipes[0].x <= -20:
+        pipes.pop(0)
+        pipes.pop(0)
+
+    if pipes[-1].x <= 200:
+        new_pipes = create_pipes()
+        pipes.extend(new_pipes)
+
+
+def draw_screen():
+    win.fill((0, 0, 0))
+
+    bird.draw(win)
+
+    for pipe in pipes:
+        pipe.update()
+        pipe.draw(win)
+
+    pg.display.flip()
+
+
+#############
+# VARIABLES #
+#############
+
+# Window
+win_width = 400
+win_height = 600
+win = pg.display.set_mode((win_width, win_height))
+pg.display.set_caption('Flappy bird')
+
+# Bird
+bird = Bird()
+
+# Pipes
+pipes = [*create_pipes()]
+
+#############
+# MAIN LOOP #
+#############
+
+run = True
+clock = pg.time.Clock()
+
+while run:
+    clock.tick(60)
+
+    for e in pg.event.get():
+        if e.type == pg.QUIT:
+            run = False
+
+    update()
+    draw_screen()
+
+pg.quit()
